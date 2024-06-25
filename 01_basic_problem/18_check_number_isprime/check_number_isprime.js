c6f2244f77e584966a2ac9c2f012b8c9a8dfea30
sonu shrivastav <sonushrivastav1811@gmail.com>
2024-06-25 20:52:40 +0530

    added check number is isprime

---
+++ 01_basic_problem/18_check_number_isprime/check_number_isprime.js
@@ -0,0 +1,14 @@
+function checkNumberIsPrime(num){
+    if(num < 1){
+        return false
+    }
+    for(let i=2;i<= Math.sqrt(num);i++){
+        if(num % i === 0){
+            return false
+        }
+    }
+
+    return true
+}
+
+module.exports = checkNumberIsPrime
---
+++ 01_basic_problem/18_check_number_isprime/check_number_isprime.run.js
@@ -0,0 +1,4 @@
+const checkNumberIsPrime = require("./check_number_isprime")
+
+const result = checkNumberIsPrime(4)
+console.log(result);
