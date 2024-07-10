91635d4d7b03f075dc14d2639d98212d7af769fc
sonu shrivastav <sonushrivastav1811@gmail.com>
2024-07-10 20:21:00 +0530

    added object is array

---
+++ 01_basic_problem/51_objectIs_array/objectIs_array.js
@@ -0,0 +1,5 @@
+function isArray(obj){
+    return Array.isArray(obj)
+}
+
+module.exports = isArray
---
+++ 01_basic_problem/51_objectIs_array/objectIs_array.run.js
@@ -0,0 +1,3 @@
+const isArray = require("./objectIs_array")
+const result = isArray([{a:"1",b:"2",c:"3",d:"4"}])
+console.log(result);
