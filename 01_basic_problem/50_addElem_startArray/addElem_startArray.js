46684fd6f11dd75b7b629ac5ec2b06fc9ab1261e
sonu shrivastav <sonushrivastav1811@gmail.com>
2024-07-10 20:13:20 +0530

    added add Elem at start

---
+++ 01_basic_problem/50_addElem_startArray/addElem_startArray.js
@@ -0,0 +1,6 @@
+function addElemAtStart(arr,elem){
+   arr.unshift(elem)
+   return arr
+}
+
+module.exports = addElemAtStart
---
+++ 01_basic_problem/50_addElem_startArray/addElem_startArray.run.js
@@ -0,0 +1,4 @@
+const addElemAtStart = require("./addElem_startArray")
+
+const result = addElemAtStart([1,2,4],6)
+console.log(result);
